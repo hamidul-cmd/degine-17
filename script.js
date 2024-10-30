e0ef4299d8404dfea033914d0b18abe4b686ffd6
Hamidul Islam Maruf <hamidulislammaruf2@gmail.com>
2024-10-30 21:07:08 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navmanu = document.querySelector(".nav-manu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navmanu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll")
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navmanu.classList.remove("active");
+    document.body.classList.remove("no-scroll")
+}))
